8ea7fada961bebf6a602bafff79496de8b187abb
Bill Neely <bill.neely.tx@gmail.com>
2014-06-27 12:56:16 -0400

    Problem 20 complete in Ruby

--- Ruby/helper.rb
+++ Ruby/helper.rb
@@ -1,5 +1,5 @@
-def time_diff_milli(start, finish)
-   (finish - start) * 1000.0
+def time_diff_in_seconds(start, finish)
+   '%.2f' % (finish - start) 
 end
 
 def printResult(myAnswer, correctAnswer)
@@ -15,9 +15,9 @@ end
 def printResultWithTimer(myAnswer, correctAnswer, startTime, endTime)
 	puts
 	if myAnswer == correctAnswer
-		puts "I found the correct answer of #{myAnswer} in #{time_diff_milli(startTime, endTime)} milliseconds"
+		puts "I found the correct answer of #{myAnswer} in #{time_diff_in_seconds(startTime, endTime)} seconds"
 	else
-		puts "I found the incorrect answer of #{myAnswer}.  It should be #{correctAnswer}. It took #{time_diff_milli(startTime, endTime)} milliseconds"
+		puts "I found the incorrect answer of #{myAnswer}.  It should be #{correctAnswer}. It took #{time_diff_in_seconds(startTime, endTime)} seconds"
 	end
 	puts
 end
---
+++ Ruby/problem020.rb
@@ -0,0 +1,28 @@
+# Factorial DIgit Sum
+
+# n! means n * (n-1) * ... * 3 * 2 * 1
+# for example 10! = 10 * 9 * 8 * ... * 2 * 1 = 3628800
+# the sume of the digits in 10! = 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27
+# Find the sun of the digits in 100!
+
+require_relative 'helper'
+
+class Integer 
+    def !
+        (1..self).inject(:*) || 1     
+    end
+    
+    def sumDigits
+        self.to_s.split('').map{ |i| i.to_i }.reduce(:+)
+    end
+end
+
+startTime = Time.now
+
+correctAnswer = 648
+myAnswer = 100.!.sumDigits
+
+endTime = Time.now
+
+printResultWithTimer(myAnswer, correctAnswer, startTime, endTime)
+
