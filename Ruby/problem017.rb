8c3e8fe1036ef383d9ce5d110809b528285bcf58
bill-neely <bneely@labattfood.com>
2014-07-03 14:23:24 -0500

    Playing with web services

--- Ruby/problem017.rb
+++ Ruby/problem017.rb
@@ -11,22 +11,54 @@
 # use of "and" when writing out numbers in in compliance with British usage
 
 require_relative('helper')
+require('net/http')
+
+ENV['http_proxy'] = 'http://72.37.249.115:8080'
 
 class Integer
     def inWords
-    	return "one" if self == 1
-    	return "two" if self == 2
-    	return "three" if self == 3
-    	return "four" if self == 4
-    	return "five" if self == 5
-    	return "six" if self == 6
-    	return "seven" if self == 7
-    	return "eight" if self == 8
-    	return "nine" if self == 9
+    	url = "http://www.dataaccess.com/webservicesserver/numberconversion.wso/NumberToWords/JSON?ubiNum=#{self}"
+    	puts Net::HTTP
+		resp = Net::HTTP.get_response(URI.parse(url)) # get_response takes an URI object
+		return resp.body
+    	#return toEnglish(self)
     end
 end
 
+def toEnglish (number)
+	return "one" if number == 1
+   	return "two" if number == 2
+   	return "three" if number == 3
+  	return "four" if number == 4
+   	return "five" if number == 5
+   	return "six" if number == 6
+   	return "seven" if number == 7
+   	return "eight" if number == 8
+   	return "nine" if number == 9
+   	return "ten" if number == 10
+   	return "eleven" if number == 11
+   	return "twelve" if number == 12
+   	return "thirteen" if number == 13
+   	return "fourteen" if number == 14
+   	return "fifteen" if number == 15
+   	return "sixteen" if number == 16
+   	return "seventeen" if number == 17
+   	return "eightteen" if number == 18
+   	return "nineteen" if number == 19
+   	return "twenty" if number == 20
+   	return "thirty" if number == 30
+   	return "forty" if number == 40
+   	return "fifty" if number == 50
+   	return "sixty" if number == 60
+   	return "seventy" if number == 70
+   	return "eighty" if number == 80
+   	return "ninety" if number == 90
+end
 
+
+puts "starting call"
+puts 23.inWords
+puts "finished call"
 #startTime = Time.now
 
 #correctAnswer = 21124
--- Ruby/problem017_spec.rb
+++ Ruby/problem017_spec.rb
@@ -1,7 +1,7 @@
 require_relative 'problem017'
 
 describe Integer do 
-	it "1 through 9" do 
+	it "unique words - 1 through 19" do 
 		expect(1.inWords).to eq("one")
 		expect(2.inWords).to eq("two")
 		expect(3.inWords).to eq("three")
@@ -11,6 +11,36 @@ describe Integer do
 		expect(7.inWords).to eq("seven")
 		expect(8.inWords).to eq("eight")
 		expect(9.inWords).to eq("nine")
+		expect(10.inWords).to eq("ten")
+		expect(11.inWords).to eq("eleven")
+		expect(12.inWords).to eq("twelve")
+		expect(13.inWords).to eq("thirteen")
+		expect(14.inWords).to eq("fourteen")
+		expect(15.inWords).to eq("fifteen")
+		expect(16.inWords).to eq("sixteen")
+		expect(17.inWords).to eq("seventeen")
+		expect(18.inWords).to eq("eightteen")
+		expect(19.inWords).to eq("nineteen")
 	end
+
+	it "unique words, 20-90 by tens" do 
+		expect(20.inWords).to eq("twenty")
+		expect(30.inWords).to eq("thirty")
+		expect(40.inWords).to eq("forty")
+		expect(50.inWords).to eq("fifty")
+		expect(60.inWords).to eq("sixty")
+		expect(70.inWords).to eq("seventy")
+		expect(80.inWords).to eq("eighty")
+		expect(90.inWords).to eq("ninety")
+	end
+
+	it "combinations under 100" do 
+		expect(23.inWords).to eq("twenty-three")
+		expect(35.inWords).to eq("thirty-five")
+		expect(47.inWords).to eq("forty-seven")
+		expect(58.inWords).to eq("fifty-eight")
+		expect(99.inWords).to eq("ninety-nine")
+	end
+
 end
 
