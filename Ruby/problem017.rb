efcd69221490971b1499504089440f123eb43d48
Bill Neely <bill.neely.tx@gmail.com>
2014-07-08 11:47:45 -0400

    Finish problem 17 Ruby

--- Ruby/problem017.rb
+++ Ruby/problem017.rb
@@ -13,71 +13,20 @@
 require_relative('helper')
 require('net/http')
 
-#ENV['http_proxy'] = 'http://72.37.249.115:8080'
-
 class Integer
     def inWords
     	url = "http://www.dataaccess.com/webservicesserver/numberconversion.wso/NumberToWords/JSON?ubiNum=#{self}"
-		proxy_addr = '72.37.249.115'
-		proxy_port = 8080
-		#service = Net::HTTP.new("http://www.dataaccess.com/webservicesserver/numberconversion.wso/NumberToWords/JSON?ubiNum=#{self}",
-		#				nil, proxy_addr, proxy_port)
-		#return service.get
-		proxy = Net::HTTP::Proxy(proxy_addr, proxy_port)
-		proxy.start(url) { |http|
-			puts http.methods
-		}
-		#Net::HTTP.new(url, nil, proxy_addr, proxy_port).start { |http|
-  				# always proxy via your.proxy.addr:8080
-  		#	resp = http.get(URI.parse(url))
-  		#	puts resp.methods
-		#}    	
- 		#resp = Net::HTTP.get_response(URI.parse(url)) # get_response takes an URI object
-		#return resp.body
+		resp = Net::HTTP.get_response(URI.parse(url)) # get_response takes an URI object
+		return resp.body
     end
 end
 
-def toEnglish (number)
-	return "one" if number == 1
-   	return "two" if number == 2
-   	return "three" if number == 3
-  	return "four" if number == 4
-   	return "five" if number == 5
-   	return "six" if number == 6
-   	return "seven" if number == 7
-   	return "eight" if number == 8
-   	return "nine" if number == 9
-   	return "ten" if number == 10
-   	return "eleven" if number == 11
-   	return "twelve" if number == 12
-   	return "thirteen" if number == 13
-   	return "fourteen" if number == 14
-   	return "fifteen" if number == 15
-   	return "sixteen" if number == 16
-   	return "seventeen" if number == 17
-   	return "eightteen" if number == 18
-   	return "nineteen" if number == 19
-   	return "twenty" if number == 20
-   	return "thirty" if number == 30
-   	return "forty" if number == 40
-   	return "fifty" if number == 50
-   	return "sixty" if number == 60
-   	return "seventy" if number == 70
-   	return "eighty" if number == 80
-   	return "ninety" if number == 90
-end
+startTime = Time.now
 
+correctAnswer = 21124
+words = (1..1000).map{ |i| i.inWords.gsub(/[^a-zA-Z]/, '').length }
+myAnswer = words.reduce(:+)
 
-(99..99).each{ |i| puts i.inWords }
-#puts "starting call"
-#puts 23.inWords
-#puts "finished call"
-#startTime = Time.now
+endTime = Time.now
 
-#correctAnswer = 21124
-#correctAnswer = "three thousand three hundred and forty-two"
-#myAnswer = 3342.inWords
-
-#endTime = Time.now
-
-#printResultWithTimer(myAnswer, correctAnswer, startTime, endTime)
+printResultWithTimer(myAnswer, correctAnswer, startTime, endTime)
